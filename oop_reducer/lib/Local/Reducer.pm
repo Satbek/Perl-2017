c99f50a72716334f6ce8810a438f5b2432807612
Satbek Turganbayev <set.turg@mail.ru>
2017-11-01 17:39:59 +0300

    Reducer.pm

--- oop_reducer/lib/Local/Reducer.pm
+++ oop_reducer/lib/Local/Reducer.pm
@@ -1,5 +1,5 @@
 package Local::Reducer;
-
+use 5.016;
 use strict;
 use warnings;
 
@@ -20,5 +20,46 @@ our $VERSION = '1.00';
 =head1 SYNOPSIS
 
 =cut
+
+sub new {
+	my ($self, %args) = @_;
+	my %data;
+	$data{source} = $args{source};
+	$data{row_class} = $args{row_class};
+	$data{reduced} = $args{initial_value};
+	return bless \%data;
+}
+
+
+sub reduce {
+	my $self = shift;
+	my $row = $self->{source}->next; 
+	if ($row) {
+		#...
+	}
+	else { return undef }
+}
+
+sub reduce_n {
+	my ($self, $n) = @_;
+	for (1..$n) {
+		$self->reduce();
+	}
+	return $self->reduced;
+}
+
+sub reduce_all {
+	my $self = shift;
+	while (1) { last unless $self->reduce() }
+	return $self->reduced;
+}
+
+sub reduced {
+	my $self = @_;
+	return $self->{reduced};
+}
+
+
+
 
 1;
