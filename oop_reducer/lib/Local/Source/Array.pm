52b3ece0e3dd769c18697449584a4d367551009d
Satbek Turganbayev <set.turg@mail.ru>
2017-11-01 15:56:57 +0300

    01 test done

--- oop_reducer/lib/Local/Source/Array.pm
+++ oop_reducer/lib/Local/Source/Array.pm
@@ -1,13 +1,14 @@
-package Array;
+package Local::Source::Array;
 use 5.016;
 use warnings;
 use DDP;
 sub new {
 	shift;
 	my %data = @_;
+	my @arr = @{$data{array}};
+	$data{array} = \@arr;
 	return bless \%data;
 }
-
 sub next {
 	my $self = shift;
 	return @{$self->{array}} ? shift @{$self->{array}} : undef;
