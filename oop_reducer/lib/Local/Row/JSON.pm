4d3fd2fbf269349e320df8f9e6a6d4fd57634dc9
Satbek Turganbayev <set.turg@mail.ru>
2017-11-01 16:15:55 +0300

    JSON passed tests

--- oop_reducer/lib/Local/Row/JSON.pm
+++ oop_reducer/lib/Local/Row/JSON.pm
@@ -1,11 +1,13 @@
-package JSON;
+package Local::Row::JSON;
 use 5.016;
 use warnings;
 use JSON::XS qw/decode_json/;
 use DDP;
 sub new {
-	my ($self, $str) = @_;
+	my ($self, %args) = @_;
+	my $str = $args{str};
 	my $data = eval{ decode_json $str };
+	return undef unless ref $data eq "HASH";
 	return $data ? bless $data : $data;
 }
 
