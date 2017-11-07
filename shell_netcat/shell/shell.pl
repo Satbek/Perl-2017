71c565b56af16913fdeb6e0efc5ad595b1928221
Satbek Turganbayev <set.turg@mail.ru>
2017-11-07 15:42:52 +0300

    fix all, except ps

--- shell_netcat/shell/shell.pl
+++ shell_netcat/shell/shell.pl
@@ -3,17 +3,69 @@ use 5.016;
 use warnings;
 use DDP;
 use Sys::Hostname;
+use Cwd qw(cwd getcwd);;
+#cd/pwd/echo/kill/ps
 $|++;
 say "hello, it's my shell!";
-my $host = hostname;
-my $prefix = "$ENV{USER}\@$host:~$ENV{PWD}\$ ";
-print $prefix;
+
+sub get_prefix {
+	my $host = hostname;
+	my $current_dir = getcwd();
+	$current_dir =~ s/^$ENV{HOME}//;
+	return "$ENV{USER}\@$host:~$current_dir\$ ";
+}
+
+sub process_command_in_child {
+	my $command = shift;
+	if ($command =~ /^echo\s*(?:(?<=\s)(?<arg>[^\s]+))?$/) {
+		my $arg = $+{arg};
+		$arg ||= "";
+		if ($arg =~ /^\$/) {	
+			my $env_arg = substr $arg, 1;
+			$arg = $ENV{$env_arg} if exists $ENV{$env_arg};
+		}
+		say $arg;
+	}
+	elsif ($command =~ /^\s*pwd\s*$/) {
+		say cwd;
+	}
+	elsif ($command =~ /^\s*kill\s+(\d+)\s*$/){
+		kill 'KILL', $1;
+	}
+	elsif ($command =~ /^\s*ps\s*$/) {
+		# By default, ps selects all processes with the same effective user ID (euid=EUID) 
+		# as the current user and associated with the same
+  #      terminal as the invoker.  It displays the process ID (pid=PID), the terminal associated
+  #       with the process (tname=TTY), the cumulated
+  #      CPU time in [DD-]hh:mm:ss format (time=TIME), and the executable name (ucmd=CMD).  Output is unsorted by default.
+		my $my_tty = readlink("/proc/$$/fd/0");
+		my $euid = $>;
+	}
+	else {
+		no warnings;
+		for my $path (split ":", $ENV{PATH}) {
+			eval { exec "$path/$command" };
+		}
+	}
+}
+
+sub change_dir {
+	my $path = shift;
+	if ($path) {
+		chdir($path);
+	}
+	else {
+		chdir;
+	};
+}
+
+print get_prefix;
 while(<>) {
 	chomp;
 	my @commands = split /\|/;
 	my ($read, $write);
 	my @buffer;
-	for (@commands) {
+	for my $command (@commands) {
 		pipe my ($parent_rdr, $child_wdr);
 		pipe my ($child_rdr, $parent_wdr);
 		$child_wdr->autoflush(1);
@@ -24,19 +76,22 @@ while(<>) {
 			close $child_wdr;
 			open (STDOUT, ">&=".fileno($parent_wdr)) or die $!;
 			open (STDIN, "<&=".fileno($parent_rdr)) or die $! ;
-			s/^\s*(.*)\s*$/$1/;
-			exec "/bin/$_" or die $!;
+			$command =~ s/^\s*(.*)\s*$/$1/;
+			process_command_in_child($command);
 			close $parent_rdr;
 			close $parent_wdr;
 			exit;
 		}
-		elsif ($pid < 0) {
+		elsif (!defined $pid) {
 			warn "error";
 			next;
 		}
 		else {
 			close $parent_rdr;
 			close $parent_wdr;
+			if ($command =~ /^cd\s*(?:(?<=\s)(?<path>[^\s]+))?$/) {
+				change_dir($+{path});
+			}
 			for (@buffer) {
 				print $child_wdr $_;
 			}
@@ -45,12 +100,13 @@ while(<>) {
 			while (<$child_rdr>) {
 				push @buffer, $_;
 			}
-			#say "process " . wait() . " killed";
 			close($child_rdr);
+			wait; #kill zombie
 		}
 	}
 	for (@buffer) {
 		print $_;
 	}
-	print $prefix;
+
+	print get_prefix;
 }
