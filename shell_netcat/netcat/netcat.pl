74fd99c347f91d2940075e3494643bec4c61799c
Satbek Turganbayev <set.turg@mail.ru>
2017-11-08 03:37:34 +0300

    netcat client

---
+++ shell_netcat/netcat/netcat.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+use 5.016;
+use warnings;
+use DDP;
+use IO::Socket;
+my ($ip, $port, $proto) = @ARGV;
+unless (defined $proto) {
+	$proto = "tcp";
+}
+
+my $socket = IO::Socket::INET->new(
+	PeerHost => $ip,
+	PeerPort => $port,
+	Proto => $proto) or die "Can't connect to $ip $!";
+
+my $pid;
+$|++;
+if (!($pid = fork)) {
+	while (<$socket>) {
+		print $_;
+	}
+}
+elsif (! defined $pid) {
+	die "fork didn't work";
+}
+else {
+	$SIG{CHLD} = sub {
+		kill 'KILL', $pid;
+		exit;
+	};
+	while (<STDIN>) {
+		print $socket $_;
+	}
+}
