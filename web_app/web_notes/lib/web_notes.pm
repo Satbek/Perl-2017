7ce0ed8ea21e9b9b4cbee57d6839e25874296c39
Satbek Turganbayev <set.turg@mail.ru>
2017-12-06 03:50:48 +0300

    triavial log_in, log_out

--- web_app/web_notes/lib/web_notes.pm
+++ web_app/web_notes/lib/web_notes.pm
@@ -1,10 +1,37 @@
 package web_notes;
 use Dancer2;
+use Dancer2::Plugin::Database;
+use Digest::CRC qw/crc64/;
+use HTML::Entities;
+use DDP;
+use 5.016;
 
 our $VERSION = '0.1';
+my $user;
+
 
 get '/' => sub {
-    template 'index' => { 'title' => 'web_notes' };
+	if (session "logged") {
+		return template 'index' => {logged => 1, user_name => $user};
+	}
+	else {
+		return template 'index';
+	}
+};
+
+
+any ['get', 'post'] => '/login' => sub {
+	redirect "/" if session("logged");
+	$user = params->{username};
+	my $password = params->{password};
+	session "logged" => true;
+	redirect '/';
+};
+
+
+get '/logout' => sub {
+	app->destroy_session;
+	redirect '/';
 };
 
 true;
