f93a08fcecf46591d91d4832c479c6b7f96e686d
Satbek Turganbayev <set.turg@mail.ru>
2017-12-18 08:16:19 +0300

    basic test 1,2 passes

--- date-overload/lib/Local/Date.pm
+++ date-overload/lib/Local/Date.pm
@@ -1,3 +1,185 @@
 package Local::Date;
+use Moose;
+use 5.016;
+use Moose::Util::TypeConstraints;
+use DDP;
+use Time::Local;
+use POSIX qw ( strftime );
+use locale;
+use POSIX qw ( locale_h );
+use List::Util qw ( any );
+use Carp qw(confess);
+use Local::DateTypes;
+
+#задаем локаль, чтобы время выводились на английском
+
+setlocale(LC_TIME, "en_US.UTF-8");
+setlocale(LC_ALL, "en_US.UTF-8");
+
+#day, month, year, hours, minutes, seconds - для компонентов даты
+
+for my $name ( qw( seconds minutes hours year month day ) ) {
+	my $builder = '_build_' . $name;
+	has $name => (
+		is => 'ro',
+		isa => "$name",
+		builder => $builder,
+		lazy => 1,
+		required => 1,
+		predicate => "_has_$name",
+		clearer => "_clear_$name",
+	);
+}
+
+sub _build_seconds {
+	my $self = shift;
+	return (gmtime( $self->epoch ))[0];
+}
+
+sub _build_minutes {
+	my $self = shift;
+	say $self->epoch;
+	return (gmtime( $self->epoch ))[1];
+}
+
+sub  _build_hours {
+	my $self = shift;
+	return (gmtime( $self->epoch ))[2];
+}
+
+sub _build_day {
+	my $self = shift;
+	return (gmtime( $self->epoch ))[3];
+}
+
+sub _build_month {
+	my $self = shift;
+	return (gmtime( $self->epoch ))[4] + 1;
+}
+
+sub _build_year {
+	my $self = shift;
+	return 1900 + (gmtime( $self->epoch ))[5];
+}
+
+#epoch - timestamp
+
+subtype 'epoch'
+	=> as 'Int'
+	=> where { $_ >= 0 }
+	=> message { "$_ can't be a seconds from 
+				00:00:00 UTC, January 1, 1970" };
+
+has 'epoch' => (
+	is => 'ro',
+	required => 1,
+	builder => '_build_epoch',
+	isa => 'epoch',
+	lazy => 1,
+	predicate => '_has_epoch',
+	writer => '_set_epoch'
+);
+
+sub _build_epoch {
+	my $self = shift;
+	my $time = timegm( $self->seconds, $self->minutes, $self->hours, 
+						$self->day, $self->month - 1, $self->year );
+	return $time;
+}
+
+
+#format
+
+subtype 'strftime_format'
+	=> as 'Str'
+	=> where { strftime ($_, gmtime) ne $_ }
+	#если strftime хоть что-то заменит, то формат подходит
+	=> message { "$_ is not strptime format" };
+
+has 'format' => (
+	is => 'rw',
+	lazy => 1,
+	isa => 'strftime_format',
+	default => "%a %b %e %H:%M:%S %Y",
+);
+
+
+#operators
+
+use overload
+	'""'		=> \&_to_string,
+	"<=>"		=> \&_compare,
+	'0+'		=> sub { shift-> epoch },
+	'+'			=> \&_add,
+	'-'			=> \&_subtract,
+	'+='		=> \&_add_assign,
+	'-='		=> \&_subtract_assign;
+#	fallback	=> 1;
+
+sub _compare {
+	my ($left, $right) = @_;
+	return $left->epoch <=> $right->epoch;
+}
+
+sub _to_string {
+	my $self = shift;
+	return strftime $self->format, gmtime ( $self->epoch ) ;
+}
+
+#todo обработку interval, исключений, и других случаев
+sub _add {
+	my ($self, $value) = @_;
+	$self->_set_epoch($self->epoch + $value);
+	return $self->epoch;
+}
+
+sub _subtract {
+	my ($self, $value) = @_;
+	$self->_set_epoch($self->epoch - $value);
+	return $self->epoch;
+}
+
+sub _add_assign {
+	my ($self, $value) = @_;
+	$self->_set_epoch($self->epoch + $value);
+	return $self;
+}
+
+sub _subtract_assign {
+	my ($self, $value) = @_;
+	$self->_set_epoch($self->epoch - $value);
+	return $self;
+}
+#проверяем правильно ли вызван конструктор.
+
+for my $attr ( qw(seconds minutes hours year month day) ) {
+	before "_build_$attr" => sub {
+		my $self = shift;
+		confess "can't get $attr, because epoch attribute was not set!"
+			unless $self->_has_epoch;
+	};
+}
+
+before "_build_epoch" => sub {
+	my $self = shift;
+	for my $date_part ( qw (seconds minutes hours year month day) ) {
+		unless (eval "\$self->_has_$date_part") {
+			confess "can't get epoch, because $date_part was not set!";
+		}
+	}
+};
+
+
+#обеспечиваем консистентность объекта
+
+after '_set_epoch' => sub {
+	my $self = shift;
+	for my $date_part ( qw( seconds minutes hours year month day ) ) {
+		eval "\$self->_clear_$date_part";
+	}
+};
+
+
+
 
 1;
--- date-overload/lib/Local/Date/Interval.pm
+++ date-overload/lib/Local/Date/Interval.pm
@@ -1,3 +1,154 @@
 package Local::Date::Interval;
+use Moose::Util::TypeConstraints;
+use Moose;
+use locale;
+use POSIX qw ( locale_h );
+use Carp qw(confess);
+use DDP;
+use FindBin;
+use lib "$FindBin::Bin/..";
+use Local::DateTypes;
+
+#задаем локаль
+setlocale(LC_TIME, "en_US.UTF-8");
+
+#определяем типы для валидации данных
+subtype 'days'
+	=> as 'Int'
+	=> where { $_ >= 0 }
+	=> message { "$_ can't be a days count" };
+
+subtype 'duration'
+	=> as 'Int'
+	=> where { $_ >= 0 }
+	=> message { "$_ can't be a duration in seconds!" };
+
+#определяем атрибуты
+for my $name ( qw( seconds minutes hours days ) ) {
+	has $name => (
+		is => 'ro',
+		isa => "$name",
+		builder => '_build_' . $name,
+		lazy => 1,
+		required => 1,
+		predicate => "_has_$name",
+		clearer => "_clear_$name",
+	);
+}
+
+has 'duration' => (
+	is => 'ro',
+	isa => 'duration',
+	lazy => 1,
+	required => 1,
+	builder => '_build_duration',
+	predicate => '_has_duration',
+	writer => '_set_duration',
+);
+
+#builders
+
+#1st constructor
+sub _build_seconds {
+	my $self = shift;
+	return ( gmtime($self->duration) )[0];
+}
+
+sub _build_minutes {
+	my $self = shift;
+	return ( gmtime($self->duration) )[1];
+}
+	
+sub _build_hours {
+	my $self = shift;
+	( gmtime($self->duration) )[2];
+}
+
+#может быть больше 31го дня
+sub _build_days {
+	my $self = shift;
+	return int ( $self->duration / 86400 );
+}
+
+
+#2nd  constructor
+sub _build_duration {
+	my $self = shift;
+	return 24 * 60 * 60 * $self->days + 60 * 60 * $self->hours +
+			60 * $self->minutes + $self->seconds;
+}
+
+#проверяем правильно ли вызваны конструкторы
+for my $attr ( qw(seconds minutes hours days) ) {
+	before "_build_$attr" => sub {
+		my $self = shift;
+		confess "can't get $attr, because duration attribute was not set!"
+			unless $self->_has_duration;
+	};
+}
+
+before "_build_duration" => sub {
+	my $self = shift;
+	for my $interval_part ( qw (seconds minutes hours days) ) {
+		unless (eval "\$self->_has_$interval_part") {
+			confess "can't get duration, because $interval_part was not set!";
+		}
+	}
+};
+
+#переопределяем интервалы
+use overload
+	'""'		=> \&_to_string,
+	"<=>"		=> \&_compare,
+	'0+'		=> sub { shift->duration },
+	'+'			=> \&_add,
+	'-'			=> \&_subtract,
+	'+='		=> \&_add_assign,
+	'-='		=> \&_subtract_assign;
+
+sub _to_string {
+	my $self = shift;
+	return $self->days." days, ".$self->hours." hours, ".$self->minutes." minutes, ".$self->seconds." seconds";
+}
+
+sub _compare {
+	my ($left, $right) = @_;
+	return $left->duration <=> $right->duration;
+}
+
+#todo обработку interval, исключений, и других случаев
+sub _add {
+	my ($self, $value) = @_;
+	$self->_set_duration($self->duration + $value);
+	return $self->duration;
+}
+
+sub _subtract {
+	my ($self, $value) = @_;
+	$self->_set_duration($self->duration - $value);
+	return $self->duration;
+}
+
+sub _add_assign {
+	my ($self, $value) = @_;
+	$self->_set_duration($self->duration + $value);
+	return $self;
+}
+
+sub _subtract_assign {
+	my ($self, $value) = @_;
+	$self->_set_duration($self->duration - $value);
+	return $self;
+}
+
+#обеспечиваем консистентность объекта
+after '_set_duration' => sub {
+	my $self = shift;
+	for my $interval_part ( qw( seconds minutes hours days ) ) {
+		eval "\$self->_clear_$interval_part";
+	}
+};
+
+1;
 
 1;
