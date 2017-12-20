3156afb5442a382dd6e249e1f364efe0f87565c1
Satbek Turganbayev <set.turg@mail.ru>
2017-12-20 21:22:40 +0300

    todo for controller

--- exam/notes/lib/notes.pm
+++ exam/notes/lib/notes.pm
@@ -4,7 +4,11 @@ use Dancer2;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'notes' };
+    template 'index';
+
 };
+
+#todo, используя классы User, Moderator, Notes обработывать запросы.
+#вся логика будет в Model, здесь будут только интерфейсы
 
 true;
