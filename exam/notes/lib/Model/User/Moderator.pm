5bbde846712b14312ce750bcbc71ec01b4637193
Satbek Turganbayev <set.turg@mail.ru>
2017-12-20 19:54:26 +0300

    Created Model classes, that will encapsulate work with database. First of all we need to write autorization and registration.

---
+++ exam/notes/lib/Model/User/Moderator.pm
@@ -0,0 +1,4 @@
+package Moderator;
+use Moose;
+use 5.016;
+use DDP;
---
+++ exam/notes/lib/Model/User/User.pm
@@ -0,0 +1,7 @@
+package User;
+#здесь будет вся работа с бд, от юзера
+use Moose;
+use 5.016;
+use DDP;
+use Dancer2;
+use Dancer2::Plugin::Database;
